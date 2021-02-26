aa46f815fdcdcf670bab86ebcb0a1b323d3fe4e3
kirandash0122 <kirandash0122@gmail.com>
2021-02-27 00:08:06 +0530

    added cors package

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors=require('cors')
 require('dotenv').config()
 const PORT = process.env.PORT || 8080
 const mURL = process.env.MONGO_URL
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({
   extended: false
 }))
 app.use(bodyParser.json())
+app.use(cors())
 mongoose.connect(mURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
