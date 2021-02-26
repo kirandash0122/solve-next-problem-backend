f9c735395e644de0ebfac6da430f20033002c135
kirandash0122 <kirandash0122@gmail.com>
2021-02-27 02:45:46 +0530

    testing

--- server.js
+++ server.js
@@ -323,7 +323,7 @@ const skipQuestion = (handle, pid, response) => {
 }
 
 app.get('/suggest/:handle/:easy/:medium/:hard/:low?/:high?', (request, response) => {
-  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   response.setHeader('Access-Control-Allow-Credentials', true);
